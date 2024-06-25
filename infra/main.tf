236a81a186a678a6e58fe1173b4f4e2cf379de4b
Uno <ptera21c@gmail.com>
2024-06-25 22:34:09 +1000

    uploading infra files

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "funcFromIAC" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-  function_name    = "myfunc"
+  function_name    = "funcFromIAC"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "func.handler"
   runtime          = "python3.8"
@@ -70,7 +70,7 @@ data "archive_file" "zip_the_python_code" {
 }
 
 resource "aws_lambda_function_url" "url1" {
-  function_name      = aws_lambda_function.myfunc.function_name
+  function_name      = aws_lambda_function.funcFromIAC.function_name
   authorization_type = "NONE"
 
   cors {
